7d4819a5c6a27772d0d35ad9e32ebffee77b1176
weihong <weihong2398@gmail.com>
2024-03-08 13:12:39 -0500

    thingsgs

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,34 +1,51 @@
-<<template>
-  <div>
-    <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+<template>
+  <div class="container"> 
   </div>
 </template>
 
-<script>
+<script setup>
+
+const APIdata = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json";
+
+ 
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
+
+async function getscores(){
+  const testscores = {
+    type: 'bar',
+    MathScores: [
+     'Apidata.sat_math_avg_score',
+  ],
   }
 }
+return resultSet.tablePivot().map(row => ({ 
+    year: parseInt(row['Apidata.sat_math_avg_score']),
+    count: parseInt(row['Apidata.sat_math_avg_score'])
+// export default {
+//   name: 'BarChart',
+//   components: { Bar },
+//   data: () => ({
+//     loaded: false,
+//     chartData: null
+//   }),
+//   async mounted () {
+//     this.loaded = false
 
+//     try {
+//       const { userlist } = await fetch({APIdata})
+//       this.chartdata = userlist
+
+//       this.loaded = true
+//     } catch (e) {
+//       console.error(e)
+//     }
+//   }
+// }
 </script>
+Chart
 
 <style lang="scss" scoped>
 
