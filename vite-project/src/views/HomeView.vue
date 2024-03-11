6b90b24cb288af434c1ba960ff784f6e3ec83ac7
koolaids21 <weihong2398@gmail.com>
2024-03-10 20:06:59 -0400

    thing

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -3,23 +3,37 @@
 </template>
 
 <script setup>
+const APIdata = 'https://data.cityofnewyork.us/resource/f9bf-2cp4.json'
 
-const APIdata = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json";
-
-const ChartData = ref(null);
+const ChartData = ref(null)
 
 import { Bar } from 'vue-chartjs'
-import { ref, onMounted } from 'vue';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { ref, onMounted } from 'vue'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
 
 const fetchdata = async () => {
   try {
-    const response = await fetch(APIdata);
-    if(!response.ok){
-      throw new Error('response not work');
+    const response = await fetch(APIdata)
+    if (!response.ok) {
+      throw new Error('response not work')
     }
+    const data = await response.json()
+    ChartData.value = data
+    Chart()
+  } catch (error) {
+    console.error('error fetching data:')
   }
 }
+
+const Chart = {}
 </script>
 Chart
 
