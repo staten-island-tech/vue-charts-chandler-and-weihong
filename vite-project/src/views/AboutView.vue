18d09172fc7f3d88db5464f95d683d682e6f3d1a
weihong <weihong2398@gmail.com>
2024-03-12 13:13:53 -0400

    zzzzz

--- vite-project/src/views/AboutView.vue
+++ vite-project/src/views/AboutView.vue
@@ -1,12 +1,8 @@
-<<template>
-  <div>
-
-  </div>
+<
+<template>
+  <div></div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -9,50 +9,60 @@ const APIdata = 'https://data.cityofnewyork.us/resource/f9bf-2cp4.json'
 const ChartData = ref(APIdata)
 
 import { ref, onMounted } from 'vue'
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+// import { Bar } from 'vue-chartjs'
+// import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+// ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-const fetchdata = async () => {
-  try {
-    const response = await fetch(APIdata)
-    if (!response.ok) {
-      throw new Error('response not work')
-    }
-    const data = await response.json()
-    ChartData.value = data
-    Chart()
-  } catch (error) {
-    console.error('error fetching data:')
-  }
+// const fetchdata = async () => {
+//   try {
+//     const response = await fetch(APIdata)
+//     if (!response.ok) {
+//       throw new Error('response not work')
+//     }
+//     const data = await response.json()
+//     ChartData.value = data
+//     console.log(data.data);
+//   } catch (error) {
+//     console.error('error fetching data:')
+//   }
+// }
+
+
+// const Chart = {
+//   name: 'Bar',
+//   Components: { Bar },
+//   data:()=>({
+//     loaded: false,
+//     chartData:null,
+  
+//   }),
+//    async mounted (){
+//     this.loaded = false
+//     try{
+//       const { userlist } = await fetch("https://data.cityofnewyork.us/resource/f9bf-2cp4.json")
+//       this.chartdata = userlist
+//       this.loaded = true
+//     } catch (e){
+//       console.error(e)
+//     }
+//   }
+// }
+
+// onMounted(()=> {
+//   ChartData.value.textContent = "{}"
+// })
+// console.log(Chart)
+const thing = ref('');
+async function score(){
+  let res = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+  let data = await res.json()
+  score.value = data.results
 }
-
-
-
-const Chart = {
-  name: 'Bar',
-  Components: { Bar },
-  data:()=>({
-    loaded: false,
-    chartData:null
-  }),
-   async mounted (){
-    this.loaded = false
-    try{
-      const { userlist } = await fetch(fetchdata)
-      this.chartdata = userlist
-      this.loaded = true
-    } catch (e){
-      console.error(e)
-    }
-  }
-}
-
-onMounted(()=> {
-  ChartData.value.textContent = 'thing'
+onMounted(()=>{
+  score()
 })
-console.log(Chart)
+console.log(APIdata.school_name)
 </script>
 Chart
 
