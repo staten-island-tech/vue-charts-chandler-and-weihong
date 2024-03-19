f7b643be89fb447a214cb463d063c229c99e8eef
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-03-19 13:02:39 -0400

    Update HomeView.vue

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+<Bar v-if="loaded" :data="ChartData"></Bar>
+  </div>
+</template>
+
+<script>
+import {Bar} from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+export default {
+  setup () {
+    
+    return {}
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
