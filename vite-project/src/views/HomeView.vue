7a4481d44928635edea1008a825024e96bd133a8
weihong <weihong2398@gmail.com>
2024-03-19 13:06:06 -0400

    API

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,6 +1,25 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref,  } from 'vue';
+import {onBeforeMount} from 'vue';
+
+const thing = ref('')
+async function thingy () {
+  try {
+  let fetchthing = await fetch ("https://data.cityofnewyork.us/resource/f9bf-2cp4.json")
+  let putasjson = await fetchthing.json();
+  thing.value = putasjson;
+  console.log(putasjson)
+}  
+catch (error) {
+  console.log ("error", error)
+}}
+onBeforeMount(() =>{
+    thingy();
+})
+
 </script>
+
 
 <template>
   <main>
