bc374e97c9161ab1a6eb5eb953752612d0fb2cd0
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-03-25 13:14:23 -0400

    starting scatter plot

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -5,12 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <BarChart />
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">BarChart</RouterLink>
-        <RouterLink to="/about">Scatter</RouterLink>
-      </nav>
-    </div>
+
   </header>
 
   <RouterView />
--- vite-project/src/views/ChandlerGraph.vue
+++ vite-project/src/views/ChandlerGraph.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-
+<Scatter v-if="loaded" :data="chartData"></Scatter>
     </div>
 </template>
 
 <script setup>
+import { Scatter } from 'vue-chartjs';
 
+async function Getter() {
+  try {
+    const retrieve = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+    const myjson = await retrieve.json()
+    const mathscores = myjson.map((item) => item.sat_math_avg_score)
+    const englishscores = myjson.map((items) => items.sat_critical_reading_avg_score)
+  
+
+} catch (error) {
+    console.log('error', error)
+  }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
