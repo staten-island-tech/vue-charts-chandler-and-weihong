6ceb44cff629bdcd7d2c1afc1cb47df4fd29f9c7
weihong <weihong2398@gmail.com>
2024-03-26 12:54:00 -0400

    pppp

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,30 +1,60 @@
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  <div>
+    <Line v-if="loaded" :data="chartData"></Line>
+  </div>
 </template>
 
-<script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+<script setup>
+import { ref, onBeforeMount } from 'vue'
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
+const chartData = ref(null)
+const loaded = ref(false)
+
+async function thingy() {
+  try {
+    const fetchthing = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+    const putasjson = await fetchthing.json()
+    const mathscores = putasjson.map((item) => item.sat_math_avg_score)
+    const englishscores = putasjson.map((items) => items.sat_critical_reading_avg_score)
+    chartData.value = {
+      labels: mathscores, 
+      datasets: [
+        {
+          label: 'MATH SCORES',
+          backgroundColor: 'blue',
+          data: mathscores,
+        },
+        {
+          label: 'ENGLISH SCORES',
+          backgroundColor: 'green',
+          data: englishscores,
+        }
+      ]
     }
+    loaded.value = true
+  } catch (error) {
+    console.log('error', error)
   }
 }
+
+onBeforeMount(() => {
+  thingy()
+})
 </script>
+
+<style scoped>
+
+</style>
